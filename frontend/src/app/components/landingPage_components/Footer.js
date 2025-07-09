c2b595c931a68bff04e7d3b36ebd0130e4803e5b
haris <harishammadpak1@gmail.com>
2025-07-09 16:28:55 +1400

    Update Footer.js

--- frontend/src/app/components/landingPage_components/Footer.js
+++ frontend/src/app/components/landingPage_components/Footer.js
@@ -65,52 +65,8 @@ export default function Footer() {
           </p>
            
         </div>
-        {/* Divider */}
-        <div className="hidden lg:block w-px bg-white/30 self-stretch" />
-        {/* Contact Info */}
-        <div className="flex-1 mb-8 lg:mb-0">
-          <h3 className="text-lg font-bold mb-4">Contact information</h3>
-          <p className="text-sm">Phone:</p>
-          <p className="text-sm mb-6"> +923289603143</p>
-          <p className="text-sm">Mail:</p>
-          <p className="text-sm mb-6">info@commentsfusion.com</p>
-          <p className="text-sm">Address:</p>
-          <p className="text-sm">NICAT, ALPHA-11, NASTP</p>
-          <p className="text-sm">Monday - Friday</p>
-          <p className="text-sm">8:00am - 5:00pm</p>
-        </div>
-        {/* Divider */}
-        <div className="hidden lg:block w-px bg-white/30 self-stretch" />
-        {/* Legals */}
-        <div className="flex-1 mb-8 lg:mb-0">
-          <h3 className="text-lg font-bold mb-4">Legals</h3>
-          <ul className="space-y-6 sm:space-y-10 text-sm">
-            <li><a href="#" >Terms & Condition</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Return & Refund</a></li>
-          </ul>
-        </div>
-        {/* Divider */}
-        <div className="hidden lg:block w-px bg-white/30 self-stretch" />
-        {/* Quick Links */}
-        <div className="flex-1">
-          <h3 className="text-lg font-bold mb-4">Quick Links</h3>
-          <ul className="space-y-3 sm:space-y-5 text-sm">
-            <li><a href="#">Affiliate Program</a></li>
-            <li><a href="#">Pricing</a></li>
-            <li><a href="#">Features</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-        </div>
-      </div>
-      {/* Bottom Text */}
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center mt-10 sm:mt-20 text-xs sm:text-sm text-white/60 px-2">
-        <p>© 2025 Comments Fusion. All rights reserved.</p>
-        <p className="mt-3 md:mt-0">
-          Design & Develop by <span className="text-white">Konquer Solution Pvt Ltd</span>
-        </p>
-      </div>
+       
+      
     </footer>
   );
 }
