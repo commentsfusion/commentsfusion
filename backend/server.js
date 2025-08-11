41f803ac052bb78c95695ddccdda2df604145e2f
ayeshanazneen <134217413+ayeshanazneen1@users.noreply.github.com>
2025-08-11 12:01:46 +0500

    trust proxy fix

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.set('trust proxy', true);
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
